c3dd9e082175ced187e5e20a782862f6260dba44
pixarninja <wesharris505@gmail.com>
2019-04-20 13:30:04 +0000

    Fiddle with Docker and Selenium

--- testing/selenium_tests.py
+++ testing/selenium_tests.py
@@ -1,6 +1,3 @@
-from time import sleep
-sleep(5)
-
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
@@ -8,17 +5,8 @@ firefox = webdriver.Remote(
           command_executor='http://54.174.152.202:4444/wd/hub',
           desired_capabilities=DesiredCapabilities.FIREFOX) 
 
-firefox.get('https://www.google.com')
+firefox.get('https://54.174.152.202/index.html')
+#firefox.get('https://www.google.com/')
 print(firefox.title)
 
 firefox.quit()
-
-def waitForResourceAvailable(response, timeout, timewait):
-    timer = 0
-    while response.status_code != 200:
-        time.sleep(timewait)
-        timer += timewait
-        if timer > timeout:
-            break
-        if response.status_code == 200:
-            break
