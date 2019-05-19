ce9741383e9b22618eea7d88e8b69af5fc0b8cb2
pixarninja <wesharris505@gmail.com>
2019-05-19 12:09:05 -0600

    Add variance data analysis

---
+++ public/tests/single/processing.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string line;
+    int j;
+    for(int i = 1; i <= 10; i++) {
+        ifstream input;
+        ofstream output;
+        
+        input.open ("User" + to_string(i) + "_outputs.dat");
+        output.open ("User" + to_string(i) + "_parsed.dat");
+        j = 1;
+        while ( getline (input,line) )
+        {
+            if(j != 1) {
+                output << ',';
+            }
+            output << line;
+            j++;
+        }
+        output.close();
+        input.close();
+    }
+    return 0;
+}
